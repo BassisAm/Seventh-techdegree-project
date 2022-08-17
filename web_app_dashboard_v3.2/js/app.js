e4e85da59a53f65bafa359a7dcc0d3abb2414d71
BassisAm <108362399+BassisAm@users.noreply.github.com>
2022-08-17 19:23:36 -0400

    Changes V2

--- web_app_dashboard_v3.2/js/app.js
+++ web_app_dashboard_v3.2/js/app.js
@@ -1,3 +1,15 @@
+
+let alertBanner = document.getElementsByClassName('alert');
+
+
+alertBanner.addEventListener('click', e => {
+    const element = e.target;
+    if (element.classList.contains("alert-banner-close")) {
+    alertBanner.style.display = "none"
+    }
+    });
+
+
 const user = document.getElementById("userField");
 const message = document.getElementById("messageField");
 const send = document.getElementById("send");
@@ -14,3 +26,76 @@ alert("Please fill out message field before sending");
 alert(`Message successfully sent to: ${user.value}`);
 }
 });
+
+
+let trafficCanvas = document.getElementById("traffic-chart");
+
+let trafficData = {
+    labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3",
+    "4-10", "11-17", "18-24", "25-31"],
+    datasets: [{
+    data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500,
+    2500],
+    backgroundColor: 'rgba(116, 119, 191, .3)',
+    borderWidth: 1,
+    }]
+    };
+
+    
+let trafficChart = new Chart(trafficCanvas, {
+    type: 'line',
+    data: trafficData
+    });
+
+
+
+
+
+
+const dailyCanvas = document.getElementById("daily-chart");
+
+const dailyData = {
+    labels: ["S", "M", "T", "W", "T", "F", "S"],
+    datasets: [{
+    label: '# of Hits',
+    data: [75, 115, 175, 125, 225, 200, 100],
+    backgroundColor: '#7477BF',
+    borderWidth: 1
+    }]
+    };
+
+let dailyChart = new Chart(dailyCanvas, {
+    type: 'bar',
+    data: dailyData,
+    });
+
+
+
+
+
+
+
+
+
+
+
+const mobileCanvas = document.getElementById("mobile-users-chart");
+
+const mobileData = {
+    labels: ["Desktop", "Tablet", "Phones"],
+    datasets: [{
+    label: '# of Users',
+    data: [2000, 550, 500],
+    borderWidth: 0,
+    backgroundColor: [
+    '#7477BF',
+    '#78CF82',
+    '#51B6C8'
+    ]
+    }]
+    };
+
+    let mobileChart = new Chart(mobileCanvas, {
+        type: 'doughnut',
+        data: mobileData,
+        });
